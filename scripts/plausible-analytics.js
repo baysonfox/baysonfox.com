43033849b85c20087dc13c54ae43aeef6592ce7e
baysonfox <baysonfox@baysonfox.com>
2023-05-13 18:34:03 +0800

    Image lazyload and analytics script change

--- scripts/plausible-analytics.js
+++ scripts/plausible-analytics.js
@@ -1,4 +1,4 @@
 hexo.extend.injector.register('head_end', 
-    '<script defer="" data-domain="baysonfox.com" data-api="https://plausible.baysonfox.com/api/event" src="https://plausible.baysonfox.com/js/script.outbound-links.file-downloads.js" type="text/javascript"></script>'
+    '<script defer="" data-domain="baysonfox.com" data-api="https://plausible.baysonfox.com/api/event" src="https://baysonfox.com/scripts/script.outbound-links.file-downloads.js" type="text/javascript"></script>'
 )
 
