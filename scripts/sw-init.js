5538e4549dcda672b95a009ed9bd58650ebdec6c
baysonfox <baysonfox@baysonfox.com>
2023-08-13 11:02:18 +0800

    Removing plausible analytics and replacing it with microsoft clarity Streamline files

--- scripts/sw-init.js
+++ scripts/sw-init.js
@@ -1,3 +1,13 @@
 hexo.extend.injector.register('head_end', 
-    '<script src="/js/custom/custom.js"></script>'
+    `<script defer type="text/javascript">
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function () {
+            navigator.serviceWorker.register('scripts/service_workers.js').then(function (registration) {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }).catch(function (err) {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    }
+    </script>`
 )
