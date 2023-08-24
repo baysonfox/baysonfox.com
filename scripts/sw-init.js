f2e791314579d4d28fc08d1493b072638a7453fb
baysonfox <baysonfox@baysonfox.com>
2023-08-24 21:06:35 +0800

    Fix service workers address

--- scripts/sw-init.js
+++ scripts/sw-init.js
@@ -2,7 +2,7 @@ hexo.extend.injector.register('head_end',
     `<script defer type="text/javascript">
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', function () {
-            navigator.serviceWorker.register('scripts/service_workers.js').then(function (registration) {
+            navigator.serviceWorker.register('https://baysonfox.com/scripts/service_workers.js').then(function (registration) {
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
             }).catch(function (err) {
                 console.log('ServiceWorker registration failed: ', err);
