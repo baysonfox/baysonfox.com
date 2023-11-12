c43b1a9e863235016ae069726a5261706a668f3d
baysonfox <baysonfox@baysonfox.com>
2023-11-12 11:48:08 +0800

    bugfix: 移除 jQuery 异步加载

--- themes/icarus/layout/common/scripts.jsx
+++ themes/icarus/layout/common/scripts.jsx
@@ -30,7 +30,7 @@ module.exports = class extends Component {
         };`;
 
         return <Fragment>
-            <script async src={cdn('jquery', '3.3.1', 'dist/jquery.min.js')}></script>
+            <script src={cdn('jquery', '3.3.1', 'dist/jquery.min.js')}></script>
             <script src={cdn('moment', '2.29.1', 'min/moment.min.js')}></script>
             <script src={cdn('moment', '2.29.1', ('locale/' + language.toLowerCase() + '.js'))}></script>
             {clipboard && <script src={cdn('clipboard', '2.0.4', 'dist/clipboard.min.js')} defer></script>}
